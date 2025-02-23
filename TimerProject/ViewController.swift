9e75fe1aa9a7a8ab8e7f7533eab3ffdfd6f19502
aslanbeyza <aslanbeyza3413@gmail.com>
2025-02-23 18:18:06 +0300

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Beyza Aslan on 23.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
