c81f8805b7bfe6fedd135625afefa5aba88e9843
Beyza Aslan <118660685+aslanbeyza@users.noreply.github.com>
2025-02-23 19:11:17 +0300

    Update ViewController.swift

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counter = 10
+        timeLabel.text="Time: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
     }
-
-
-}
-
+        
+     @objc func timerFunction() {
+         timeLabel.text="Time: \(counter)"
+         counter -= 1
+         if counter == 0 {
+             timer.invalidate() //Timer ı bitirir durdurur
+             timeLabel.text="Time's Up!"
+         }
+        }
+        
+        @IBAction func backClickedButton(_ sender: Any) {
+            print("butona tıklandı");
+        }
+        
+    }
+    
